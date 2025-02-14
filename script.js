1e94e5362f1ab8e5b0095a75e7895fce485d1e26
Camilla <camilla.e.soderman@gmail.com>
2025-02-14 22:44:50 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+
+function myFunctionEmail() {
+    var popup = document.getElementById("myPopupEmail");
+    popup.classList.toggle("show");
+  }
+ function myFunctionSubmit() {
+    var popup = document.getElementById("submitemail")
+    
+ }
